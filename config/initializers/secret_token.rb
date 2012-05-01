204dfa6b5990b4c787728da894f52dc4f2da7ea8
Tom Vidolov <tom@vocaltec.com>
2012-05-01 16:23:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TomProject::Application.config.secret_token = '4587ebbe747bfa001bdbbeddb63e029aa696bad67cb2a724e9f910ddb9aa66de0f194ed6e2ba22285f38f8ffa6cada08017b36d42a12ff57683894ba342ec031'
